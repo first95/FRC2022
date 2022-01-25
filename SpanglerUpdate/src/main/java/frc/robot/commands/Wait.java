9f28116d2ddc8f55ab1011e5b3d4698917db68bd
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2022-01-24 20:42:06 -0500

    Need to add drivebase stop after following

---
+++ SpanglerUpdate/src/main/java/frc/robot/commands/Wait.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class Wait extends CommandBase {
+  private long startTime = 0;
+  private long endTime = 0;
+  private long time;
+  public Wait(double seconds) {
+    time = (long) (seconds * 1000);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startTime = System.currentTimeMillis();
+    endTime = startTime + time;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (endTime <= System.currentTimeMillis()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
