4f6a066007c6d50287d54a235b8fe7d70e27d9d0
Shared02 <lindsayvallen@gmail.com>
2022-02-18 19:27:05 -0500

    feat: auto climb

--- Lamarr/src/main/java/frc/robot/subsystems/Climber.java
+++ Lamarr/src/main/java/frc/robot/subsystems/Climber.java
@@ -4,12 +4,16 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Climber_Properties;
 
@@ -18,11 +22,15 @@ public class Climber extends SubsystemBase {
   private Solenoid pistons;
   private CANSparkMax leftLead;
   private CANSparkMax rightLead;
+  private SparkMaxPIDController leftController, rightController;
 
   public Climber() {
     pistons = new Solenoid(PneumaticsModuleType.REVPH, Climber_Properties.CLIMBER_PNEUMATICS_ID);
     leftLead = new CANSparkMax(Climber_Properties.LEFT_LEAD, MotorType.kBrushless);
     rightLead = new CANSparkMax(Climber_Properties.RIGHT_LEAD, MotorType.kBrushless);
+
+    leftController = leftLead.getPIDController();
+    rightController = rightLead.getPIDController();
   }
 
   /**
@@ -36,6 +44,53 @@ public class Climber extends SubsystemBase {
   public void setSpeed(double speed) {
     leftLead.set(speed);
     rightLead.set(-speed);
+  }
+
+  public void travelDistance(double rotations) {
+    leftController.setReference(rotations, com.revrobotics.CANSparkMax.ControlType.kPosition);
+    rightController.setReference(rotations, com.revrobotics.CANSparkMax.ControlType.kPosition);
+    SmartDashboard.putNumber("SetPoint", rotations);
+  }
+
+  public void applyPositionPidConsts() {
+
+    // Display PID coefficients on SmartDashboard
+    // SmartDashboard.putNumber("P Gain", kP);
+    // SmartDashboard.putNumber("I Gain", kI);
+    // SmartDashboard.putNumber("D Gain", kD);
+    // SmartDashboard.putNumber("I Zone", kIz);
+    // SmartDashboard.putNumber("Feed Forward", kFF);
+    // SmartDashboard.putNumber("Max Output", kMaxOutput);
+    // SmartDashboard.putNumber("Min Output", kMinOutput);
+
+    // Read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+
+    // If PID coefficients on SmartDashboard have changed, write new values to
+    // controller
+    leftController.setP(p);
+    rightController.setP(p);
+
+    leftController.setI(i);
+    rightController.setI(i);
+
+    leftController.setD(d);
+    rightController.setD(d);
+
+    leftController.setIZone(iz);
+    rightController.setIZone(iz);
+
+    leftController.setFF(ff);
+    rightController.setFF(ff);
+
+    leftController.setOutputRange(min, max);
+    rightController.setOutputRange(min, max);
   }
 
   @Override
