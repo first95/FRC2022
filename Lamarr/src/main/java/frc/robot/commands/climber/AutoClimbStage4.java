b86535a7ca358ae8e5b80625c7ee3347509d1706
Shared02 <lindsayvallen@gmail.com>
2022-02-28 18:17:37 -0500

    removed commented code

--- Lamarr/src/main/java/frc/robot/commands/climber/AutoClimbStage4.java
+++ Lamarr/src/main/java/frc/robot/commands/climber/AutoClimbStage4.java
@@ -42,18 +42,5 @@ public class AutoClimbStage4 extends SequentialCommandGroup {
             addCommands(new WaitUntilCommand(climber.hasLeftReachedReference(-75)));
             RobotContainer.oi.ClimberStageFourSteps[2] = 1;
         }
-        // if (RobotContainer.oi.ClimberStageFourSteps[3] == 0) { 
-        //     addCommands(new InstantCommand(() -> {
-        //         climber.togglePistons();
-        //     }));
-        //     RobotContainer.oi.ClimberStageFourSteps[3] = 1;
-        // }
-        // if (RobotContainer.oi.ClimberStageFourSteps[4] == 0) { 
-        //     addCommands(new InstantCommand(() -> {
-        //         climber.travelDistance(0);
-        //     }));
-        //     addCommands(new WaitUntilCommand(climber.hasLeftReachedReference(0)));
-        //     RobotContainer.oi.ClimberStageFourSteps[4] = 1;
-        // }
     }
 }
