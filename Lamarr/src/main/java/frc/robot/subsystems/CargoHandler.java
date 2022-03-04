dedba443449f7018e77b27265d8912646aed6df0
Shared01 <someone@example.com>
2022-03-04 10:48:39 -0500

    Sensor sensitivity fix

--- Lamarr/src/main/java/frc/robot/subsystems/CargoHandler.java
+++ Lamarr/src/main/java/frc/robot/subsystems/CargoHandler.java
@@ -34,7 +34,7 @@ public class CargoHandler extends SubsystemBase {
 
   private boolean isRed;
 
-  private final int SINGULATOR_EMPTY = 200;
+  private final int SINGULATOR_EMPTY = 100;
 
   private Alliance currentAlliance;
 
