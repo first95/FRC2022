8cdb41192b4f31b3027dbf0b774f79ab94788e0a
Shared01 <someone@example.com>
2022-03-04 10:29:50 -0500

    UnAdjusted paths for good carpet

--- Lamarr/src/main/java/frc/robot/Constants.java
+++ Lamarr/src/main/java/frc/robot/Constants.java
@@ -141,7 +141,7 @@ public final class Constants {
 		public static final double SHOOTING_INDEXER_SPEED = 0.4;
 		public static final double SHOOTER_SLOW_SPEED = 2700;
 		public static final double COLLECTOR_REVERSE = -1;
-		public static final double SHOOTING_HIGH_SPEED = 3200;
+		public static final double SHOOTING_HIGH_SPEED = 3100;
 		public static final double SHOOTING_LOW_SPEED = 2000;
 		public static final double SHOOTER_IDLE_SPEED = 2000;
 
