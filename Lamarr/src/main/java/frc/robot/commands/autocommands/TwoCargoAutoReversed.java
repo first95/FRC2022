3485ba68d93b8a0ad84be3251a7f3e2a2793ea30
95 Laptop 1 <na.intj@gmail.com>
2022-03-27 08:12:23 -0400

    Made sure the reversed two cargo leaves the tarmac

--- Lamarr/src/main/java/frc/robot/commands/autocommands/TwoCargoAutoReversed.java
+++ Lamarr/src/main/java/frc/robot/commands/autocommands/TwoCargoAutoReversed.java
@@ -32,7 +32,7 @@ public class TwoCargoAutoReversed extends SequentialCommandGroup {
         back = TrajectoryGenerator.generateTrajectory(
             new Pose2d(new Translation2d(0, 0), new Rotation2d(0)),
             List.of(),
-            new Pose2d(new Translation2d(-1, 0), new Rotation2d(0)),
+            new Pose2d(new Translation2d(-2, 0), new Rotation2d(0)),
              config);
 
 
